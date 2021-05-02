08ddf8dcb2bdfb5f006a1c06520c1db94ec0246a
Neal Huang <huangtzuchao@gmail.com>
2021-05-02 17:26:46 -0500

    added add/edit/delete problems

--- angular-8-registration-login-example/src/app/_services/problem.service.ts
+++ angular-8-registration-login-example/src/app/_services/problem.service.ts
@@ -1,18 +1,33 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Observable } from 'rxjs';
 
-import { Problem } from '@/_models';
+import { Problem, User } from '@/_models';
 
 @Injectable({ providedIn: 'root' })
 export class ProblemService {
-    constructor(private http: HttpClient) { }
+    private currentUserSubject: BehaviorSubject<User>;
+    public currentUser: Observable<User>;
+
+    constructor(private http: HttpClient) {
+        this.currentUserSubject = new BehaviorSubject<User>(JSON.parse(localStorage.getItem('currentUser')));
+        this.currentUser = this.currentUserSubject.asObservable();
+    }
 
     getAllProblems() {
         return this.http.get<Problem[]>(`${config.apiUrl}/problems`);
     }
 
     addProblem(problem: Problem) {
+        problem.owner = this.currentUserSubject.value.username;
+        problem.private = false;
         return this.http.post(`${config.apiUrl}/problems/addProblem`, problem);
+    }
+
+    editProblem(problem: Problem, id: number) {
+        problem.owner = this.currentUserSubject.value.username;
+        problem.private = false;
+        return this.http.put(`${config.apiUrl}/problems/${id}`, problem);
     }
 
     deleteProblem(id: number) {
--- angular-8-registration-login-example/src/app/home/home.component.ts
+++ angular-8-registration-login-example/src/app/home/home.component.ts
@@ -1,24 +1,41 @@
 ﻿import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { first } from 'rxjs/operators';
 
 import { User } from '@/_models';
-import { ProblemService, AuthenticationService } from '@/_services';
+import { AlertService, ProblemService, AuthenticationService } from '@/_services';
 
 @Component({ templateUrl: 'home.component.html' })
 export class HomeComponent implements OnInit {
+    problemForm: FormGroup;
+    loading = false;
+    submitted = false;
+    returnUrl: string;
     currentUser: User;
     problems = [];
 
     constructor(
+        private formBuilder: FormBuilder,
+        private router: Router,
         private authenticationService: AuthenticationService,
-        private problemService: ProblemService
+        private problemService: ProblemService,
+        private alertService: AlertService
     ) {
         this.currentUser = this.authenticationService.currentUserValue;
     }
 
     ngOnInit() {
+        this.problemForm = this.formBuilder.group({
+            name: ['', Validators.required],
+            operations: ['', Validators.required],
+            submitType: ['', Validators.required],
+        });
+
         this.loadAllProblems();
     }
+
+    get f() { return this.problemForm.controls; }
 
     deleteProblem(id: number) {
         this.problemService.deleteProblem(id)
@@ -30,5 +47,46 @@ export class HomeComponent implements OnInit {
         this.problemService.getAllProblems()
             .pipe(first())
             .subscribe(problems => this.problems = problems);
+    }
+
+    onSubmit() {
+        this.submitted = true;
+
+        // reset alerts on submit
+        this.alertService.clear();
+
+        // stop here if form is invalid
+        if (this.problemForm.invalid) {
+            return;
+        }
+
+        if (this.problemForm.value.submitType == 'add') {
+            this.problemService.addProblem(this.problemForm.value)
+            .pipe(first())
+            .subscribe(
+                data => {
+                    this.alertService.success('Problem submission successful', true);
+                    this.loadAllProblems();
+                },
+                error => {
+                    this.alertService.error(error);
+                    this.loading = false;
+                });
+        }
+        else if (this.problemForm.value.submitType == 'edit') {
+            var id = this.problems.find(element => element.name == this.problemForm.value.name).id;
+
+            this.problemService.editProblem(this.problemForm.value, id)
+            .pipe(first())
+            .subscribe(
+                data => {
+                    this.alertService.success('Problem edit successful', true);
+                    this.loadAllProblems();
+                },
+                error => {
+                    this.alertService.error(error);
+                    this.loading = false;
+                });
+        }
     }
 }
